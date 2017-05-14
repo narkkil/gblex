129d872f3f97efaac957144ec2861d184b6724b7
Nicolas Arkkila <nicolas@arkkila.fi>
2017-05-14 17:13:07 +0300

    Remove unnecessary configs and comment out default configs
    Mix tasks will fail if configs do not exist, preventing some surprises.

--- config/config.exs
+++ config/config.exs
@@ -27,4 +27,7 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-import_config "#{Mix.env}.exs"
+
+# Uncomment and configure these before starting with your blog!
+#config :gblex, github_username: "foo"
+#config :gblex, blog_title: "Foo's blog"
